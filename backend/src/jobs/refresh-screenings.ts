f4222c84a4bd86f230c7d6342e6501ae2f735a93
Thibault Lacroux <thibault.lacroux@gmail.com>
2019-12-31 17:34:08 +0100

    fix: movie job refresh

--- backend/src/jobs/refresh-screenings.ts
+++ backend/src/jobs/refresh-screenings.ts
@@ -2,4 +2,4 @@ import { schedule } from 'node-cron'
 
 import { refreshMoviesFromAllocine } from '../lib/allocine-screenings'
 
-schedule('0 0 4 * * 3', () => refreshMoviesFromAllocine('P1140'))
+schedule('0 0 4 * * 3', () => refreshMoviesFromAllocine())
